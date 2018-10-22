59d70ca1848e8a0547c113087d43b1b3dbe5bde2
higuri <higuri36@gmail.com>
2018-10-22 23:04:27 +0900

    Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # debsetup
-My setup scripts for Debian
+My setup scripts for Debian and Ubuntu (including Ubuntu on WSL). 
--- setup.sh
+++ setup.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 #
 # How to Execute:
-# $ su
-# $ apt update
-# $ apt install git
-# $ exit
+# $ su root
+# # apt update
+# # apt -y install sudo
+# # add user ${username} sudo
+# # apt install git
+# # exit
 # $ git clone https://github.com/higuri/debsetup
 # $ pushd debsetup
 # $ . .install.sh
 # $ popd
 # $ rm -r debsetup
 #
+# initialize passwd for su (Ubuntu)
+# $ sudo su -
+# # passwd
+# # ...
+# # exit
+#
+
+username=higuri
 
 # install packages
-# TODO: istall VBox GuestAdditions
-su
-apt update
-apt upgrade
-apt -y install vim
-apt -y install screen
-apt -y install python3
-exit    # su -> user
-#apt -y install git
+sudo apt update
+sudo apt upgrade
+# sudo
+# zsh
+sudo apt -y install zsh
+sudo chsh -s $(which zsh) ${username}
+# vim, tmux, python3
+sudo apt -y install vim-nox
+sudo apt -y install tmux
+sudo apt -y install python3
+sudo apt autoremove
 
 # gnome settings
 # TODO: gnome-terminal profile
 #       - color scheme, pallete (solarized)
 #       - font (M plus)
 #       - full screen, no menubar
-pushd gnome
-python gnome/set_customshortcut.py
-popd
+# pushd gnome
+# python gnome/set_customshortcut.py
+# popd
 
 # setup dotfiles
 pushd ~
