3ebbe06fcec11d452b6f08a0967a25bb862ce3a6
higuri <higuri36@gmail.com>
2018-10-23 23:04:04 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -22,20 +22,19 @@
 
 username=higuri
 
-# install packages
+# apt
 sudo apt update
-sudo apt upgrade
-# sudo
-# zsh
-sudo apt -y install zsh
-sudo chsh -s $(which zsh) ${username}
-# vim, tmux, python3
-sudo apt -y install vim-nox
-sudo apt -y install tmux
-sudo apt -y install python3
-sudo apt autoremove
-
-# gnome settings
+sudo apt -y upgrade
+# LANG
+sudo apt -y install language-pack-ja
+sudo apt -y install fonts-ipafont
+sudo update-locale LANG=ja_JP.UTF-8
+# gnome-terminal
+# for WSL:
+#  1. (win) install VcXsrv
+#  2. (win) run "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
+#  3. (deb) DISPLAY=localhost:0 gnome-terminal -e uim-fep
+# - 
 # TODO: gnome-terminal profile
 #       - color scheme, pallete (solarized)
 #       - font (M plus)
@@ -43,6 +42,23 @@ sudo apt autoremove
 # pushd gnome
 # python gnome/set_customshortcut.py
 # popd
+sudo apt -y install gnome-terminal dbus-x11
+dbus-uuidgen | sudo tee /etc/machine-id
+# uim
+sudo apt -y install uim-fep uim-anthy
+cat << EOF > ~/.uim
+(define default-im-name 'anthy)
+(define-key generic-on-key? '("<Control> "))
+(define-key generic-off-key? '("<Control> "))
+EOF
+# zsh
+sudo apt -y install zsh
+sudo chsh -s $(which zsh) ${username}
+# vim, tmux, python3
+sudo apt -y install vim-nox
+sudo apt -y install tmux
+sudo apt -y install python3
+sudo apt -y autoremove
 
 # setup dotfiles
 pushd ~
